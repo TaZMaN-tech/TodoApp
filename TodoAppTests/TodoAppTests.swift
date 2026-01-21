de6316bb7089a03d5bab2e9986e9e1a384831746
TaZMaN-tech <tazman311@gmail.com>
2026-01-21 20:47:21 +0300

    Initial Commit

---
+++ TodoAppTests/TodoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppTests.swift
+//  TodoAppTests
+//
+//  Created by Тадевос Курдоглян on 21.01.2026.
+//
+
+import Testing
+@testable import TodoApp
+
+struct TodoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
